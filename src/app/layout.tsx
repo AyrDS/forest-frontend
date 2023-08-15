d9d9f3fa9086340c662d36054418d85cfd07227c
Ayrds <ayrtonds97@gmail.com>
2023-08-15 18:37:40 -0300

    Cambios en el favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Raleway } from 'next/font/google';
 import { Navbar } from '@/components';
 
 import './globals.css';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Fork Forest',
@@ -18,6 +19,15 @@ const raleway = Raleway({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='es' className={raleway.className}>
+      <Head>
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        {/* <meta name='theme-color' content='#ffffff'></meta> */}
+      </Head>
       {/* <body>
         <Navbar />
         {children}
